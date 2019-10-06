64b4e273b3fd54993f276beadbad3c93a808b286
adamsandwich <adamsandwich@outlook.com>
2019-10-06 12:55:25 +0800

    :sparkles: update example

--- command.example.js
+++ command.example.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv').config();
 
 const DIST = process.env.DIST;
 const REPO = process.env.REPO;
+const THEME = `${REPO}/themes/${process.env.THEME}`;
+
 const CURRENT_BRANCH = `git symbolic-ref --short -q HEAD`;
 
 module.exports = function () {
@@ -12,27 +14,42 @@ module.exports = function () {
         shell.exit(1);
     }
 
-    // empty dist
-    console.log(`${chalk.green('[exec]')} clean dist`);
-    shell.rm('-rf', DIST);
-
     // before install
     shell.cd(REPO);
-    if (shell.exec(CURRENT_BRANCH).stdout.trim() !== 'jekyll') {
+    if (shell.exec(CURRENT_BRANCH).stdout.trim() !== 'master') {
         console.log(`${chalk.red('[error]')} Wrong branch`);
         shell.exit(1);
     }
+
+    console.log(`${chalk.green('[exec]')} pull the code`);
     shell.exec('git pull');
+    shell.exec('git submodule update');
+
     console.log(`${chalk.green('[exec]')} install all dependencies`);
-    if (shell.exec('bundle install').code !== 0) {
-        console.log(`${chalk.red('[error]')} bundle install error`);
+    // theme build
+    shell.cd(THEME);
+    console.log(`${chalk.green('[exec]')} theme build`);
+    if (shell.exec('yarn').code !== 0) {
+        console.log(`${chalk.red('[error]')} theme install error`);
     }
-    console.log(`${chalk.green('[exec]')} build`);
-    if (shell.exec('jekyll build').code !== 0) {
-        console.log(`${chalk.red('[error]')}  jekyll build error`);
+    if (shell.exec('yarn build').code !== 0) {
+        console.log(`${chalk.red('[error]')} theme build error`);
     }
+    // project build
+    shell.cd(REPO);
+    console.log(`${chalk.green('[exec]')} project build`);
+    if (shell.exec('yarn').code !== 0) {
+        console.log(`${chalk.red('[error]')} project install error`);
+    }
+    if (shell.exec('yarn build').code !== 0) {
+        console.log(`${chalk.red('[error]')} project build error`);
+    }
+
+    // empty dist
+    console.log(`${chalk.green('[exec]')} clean dist`);
+    shell.rm('-rf', `${DIST}/*`);
 
     // transfer file
     console.log(`${chalk.green('[exec]')} build ==> dist`);
-    shell.cp('-r', `${REPO}/_site/`, `${DIST}/`);
+    shell.cp('-r', `${REPO}/public/`, `${DIST}/`);
 }
