408b29945a6a9bf37c5a6dc95ab8d878a06f84bc
adamsandwich <adamsandwich@outlook.com>
2019-11-22 20:09:08 +0800

    :heavy_plus_sign: feat: express

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const http = require('http');
 const dotenv = require('dotenv').config();
 const crypto = require('crypto');
 const chalk = require('chalk');
 const command = require('./command');
 const log = require('fancy-log');
+const express = require('express');
+const app = express();
 
 const SECRET = process.env.SECRET;
-const URL = process.env.URL;
 
-http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end('{}');
-    if (request.headers['x-github-event'] && request.headers['x-github-event'] === 'push') {
+app.use(express.json())
+
+app.post('/webhook', function (request, response) {
+    response.json({
+        status: 0,
+    });
+    if (request.header('x-github-event') && request.header('x-github-event') === 'push') {
         log(`${chalk.green('[github-event]')} push`);
-
-        request.on('data', function (chunk) {
-            const SIGNATURE = request.headers['x-hub-signature'];
-            if (SIGNATURE == sign(SECRET, chunk.toString()) &&
-                URL == request.url) {
-                log(`${chalk.green('[verify]')} successful`);
-                Promise.resolve().then(() => command());
-            } else {
-                log(`${chalk.red('[verify]')} failed`);
-            }
-        });
+        const SIGNATURE = request.header('x-hub-signature');
+        if (SIGNATURE == sign(SECRET, JSON.stringify(request.body))) {
+            log(`${chalk.green('[verify]')} successful`);
+            Promise.resolve().then(() => command());
+        } else {
+            log(`${chalk.red('[verify]')} failed`);
+        }
     }
-}).listen(6606, '127.0.0.1');
+});
+
+app.listen(6606, function () {
+    console.log('github-webhook listening on port 6606!');
+});
 
 function sign(secret, data) {
     return 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex');
